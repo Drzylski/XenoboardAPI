ad63d08ac08462e61f60427caf711eb0d3326a54
Drzylski <drzylski@hotmail.com>
2025-05-13 15:15:34 -0500

    Uploading initial project

---
+++ src/main/java/com/shinra/xeno/XenoboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.shinra.xeno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XenoboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XenoboardApiApplication.class, args);
+	}
+
+}
