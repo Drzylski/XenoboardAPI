b10e8600b13d01118387195222e45f8a34003ee5
Zylski <48303266+Zylski@users.noreply.github.com>
2025-05-13 15:54:18 -0500

    Add info comment to main file. Add files to git ignore

--- src/main/java/com/shinra/xeno/XenoboardApiApplication.java
+++ src/main/java/com/shinra/xeno/XenoboardApiApplication.java
@@ -1,12 +1,22 @@
 package com.shinra.xeno;
 
+/*
+ * Programmer: Damian Zylski
+ * Date: 5/12/25
+ * System: Windows 10 - Eclipse
+ * 
+ * Purpose: Xenoboard is a message board application that caters to the Xeno category of games or video games in general. Api is a Java springboot Eclipse app. 
+ */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class XenoboardApiApplication {
+public class XenoboardApiApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(XenoboardApiApplication.class, args);
 	}
 
