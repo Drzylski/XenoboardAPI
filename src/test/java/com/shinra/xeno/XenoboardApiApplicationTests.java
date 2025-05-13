17ba933bfe80c68fb59195003bb1585d5d48306b
Drzylski <drzylski@hotmail.com>
2025-05-13 15:15:34 -0500

    Uploading initial project

---
+++ src/test/java/com/shinra/xeno/XenoboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinra.xeno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XenoboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
