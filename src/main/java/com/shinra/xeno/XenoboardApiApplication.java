593ff6022a2c33af80f53e5958c8a98ccffa797b
Zylski <48303266+Zylski@users.noreply.github.com>
2025-05-14 14:41:55 -0500

    Remove exclamations.

--- src/main/java/com/shinra/xeno/XenoboardApiApplication.java
+++ src/main/java/com/shinra/xeno/XenoboardApiApplication.java
@@ -3,7 +3,7 @@ package com.shinra.xeno;
 /*
  * Programmer: Damian Zylski
  * Date: 5/12/25
- * System: Windows 10 - Eclipse!
+ * System: Windows 10 - Eclipse!!!!
  * 
  * Purpose: Xenoboard is a message board application that caters to the Xeno category of games or video games in general. API is a Java springboot Eclipse app. 
  */
@@ -17,7 +17,6 @@ public class XenoboardApiApplication
 
 	public static void main(String[] args) 
 	{
-		//!!!
 		SpringApplication.run(XenoboardApiApplication.class, args);
 	}
 
